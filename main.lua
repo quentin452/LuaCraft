f38270bf6a02b8df931ac66a81a45360c7497d74
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-02-20 21:34:33 +0100

    Move src from Love2DTesting
    https://github.com/quentin452/Love2DTesting

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print('Hello World!', 1280, 720)
+end
+
+function love.keypressed(Pkey)
+
+end
