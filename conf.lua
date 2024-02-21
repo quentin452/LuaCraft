246ac90935ce0223a10553cee8b58edd9ec72136
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-02-21 14:24:14 +0100

    Fix can See-Through Polygons

--- conf.lua
+++ conf.lua
@@ -1,8 +1,9 @@
 function love.conf(t)
-    t.window.width = 1024
-    t.window.height = 576
-    t.window.minwidth = 1024
-    t.window.minheight = 576
-    t.window.title = "g3d voxel engine"
-    t.window.resizable = true
+	t.window.width = 1024
+	t.window.height = 576
+	t.window.minwidth = 1024
+	t.window.minheight = 576
+	t.window.title = "g3d voxel engine"
+	t.window.resizable = true
+	t.window.depth = 16
 end
