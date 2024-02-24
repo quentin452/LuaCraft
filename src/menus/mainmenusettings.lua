quentin452/LuaCraft

commit fbfb5bfccf78fca05c550751eb921e0c83815903

made #15   Add Logging disabler settings (prints are disabled by default)
+ reorganize some things + bugfix on vsync settings
+ optimize settings handler
+ fix #5

src/menus/mainmenusettings.lua
@@ -4,11 +4,13 @@ _MainMenuSettings.y = 50
 _MainMenuSettings.title = "Settings"
 _MainMenuSettings.choice = {}
 _MainMenuSettings.choice[1] = "Enable Vsync?"
-_MainMenuSettings.choice[2] = "Render Distance"
-_MainMenuSettings.choice[3] = "Exiting to main menu"
+_MainMenuSettings.choice[2] = "Enable Print Logging?"
+_MainMenuSettings.choice[3] = "Render Distance"
+_MainMenuSettings.choice[4] = "Exiting to main menu"
 _MainMenuSettings.selection = 0 -- initialize to 0 to prevent unwanted object selection
 
 function drawMainMenuSettings()
+	LuaCraftPrint("test")
 	local w, h = lg.getDimensions()
 	local scaleX = w / mainMenuSettingsBackground:getWidth()
 	local scaleY = h / mainMenuSettingsBackground:getHeight()
@@ -34,11 +36,17 @@ function drawMainMenuSettings()
 		local choiceText = _MainMenuSettings.choice[n]
 		if n == 1 then
 			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%d)")
-			if vsyncValue and tonumber(vsyncValue) == 1 then
+			if vsyncValue and  printValue:lower() == "true" then
 				choiceText = choiceText .. " X"
 			end
 		end
 		if n == 2 then
+			local printValue = love.filesystem.read("config.conf"):match("luacraftprint=(%w+)")
+			if printValue and printValue:lower() == "true" then
+				choiceText = choiceText .. " X"
+			end
+		end
+		if n == 3 then
 			local renderdistancevalue = love.filesystem.read("config.conf"):match("renderdistance=(%d)")
 			if renderdistancevalue then
 				choiceText = choiceText .. " " .. globalRenderDistance
@@ -69,8 +77,10 @@ function keysinitMainMenuSettings(k)
 			if _MainMenuSettings.selection == 1 then
 				toggleVSync()
 			elseif _MainMenuSettings.selection == 2 then
-				renderdistanceSetting()
+				printSettings()
 			elseif _MainMenuSettings.selection == 3 then
+				renderdistanceSetting()
+			elseif _MainMenuSettings.selection == 4 then
 				gamestate = "MainMenu"
 				_MainMenuSettings.selection = 0
 			end

src/menus/playinggamesettings.lua
@@ -4,8 +4,9 @@ _PlayingGameSettings.y = 50
 _PlayingGameSettings.title = "Settings"
 _PlayingGameSettings.choice = {}
 _PlayingGameSettings.choice[1] = "Enable Vsync?"
-_PlayingGameSettings.choice[2] = "Render Distance"
-_PlayingGameSettings.choice[3] = "Exiting to game"
+_PlayingGameSettings.choice[2] = "Enable Print Logging?"
+_PlayingGameSettings.choice[3] = "Render Distance"
+_PlayingGameSettings.choice[4] = "Exiting to game"
 _PlayingGameSettings.selection = 0 -- initialize to 0 to prevent unwanted object selection
 
 function drawPlayingMenuSettings()
@@ -35,11 +36,17 @@ function drawPlayingMenuSettings()
 		-- Ajout de la condition pour activer ou désactiver le "X"
 		if n == 1 then
 			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%d)")
-			if vsyncValue and tonumber(vsyncValue) == 1 then
+			if vsyncValue and printValue:lower() == "true" then
 				choiceText = choiceText .. " X"
 			end
 		end
 		if n == 2 then
+			local printValue = love.filesystem.read("config.conf"):match("luacraftprint=(%w+)")
+			if printValue and printValue:lower() == "true" then
+				choiceText = choiceText .. " X"
+			end
+		end
+		if n == 3 then
 			local renderdistancevalue = love.filesystem.read("config.conf"):match("renderdistance=(%d)")
 			if renderdistancevalue then
 				choiceText = choiceText .. " " .. globalRenderDistance
@@ -69,9 +76,11 @@ function keysinitPlayingMenuSettings(k)
 		elseif k == "return" then
 			if _PlayingGameSettings.selection == 1 then
 				toggleVSync()
-			elseif _PlayingGameSettings.selection == 2 then
-				renderdistanceSetting()
+			elseif _MainMenuSettings.selection == 2 then
+				printSettings()
 			elseif _PlayingGameSettings.selection == 3 then
+				renderdistanceSetting()
+			elseif _PlayingGameSettings.selection == 4 then
 				gamestate = "PlayingGame"
 				_PlayingGameSettings.selection = 0
 			end

main.lua
@@ -17,6 +17,7 @@ require("src/menus/worldcreationmenu")
 require("src/client/hud/gamehud")
 --utils
 require("src/utils/usefull")
+require("src/utils/settingshandling")
 --world
 require("src/world/chunk/chunk")
 require("src/world/scene/gamescenecore")
@@ -24,43 +25,6 @@ require("src/world/scene/gamescenecore")
 require("src/modloader/structuremodloader")
 require("src/modloader/modloaderinit")
 require("src/modloader/functiontags")
--- ModsRequireIteration
--- Specify the path to the mods directory
-local modsDirectory = "mods/"
-
--- Print debugging information
-print("Checking mods directory:", modsDirectory)
-
--- Iterate over all items in the mods directory
-local items = love.filesystem.getDirectoryItems(modsDirectory)
-for _, item in ipairs(items) do
-	local fullPath = modsDirectory .. item
-
-	-- Print debugging information
-	print("Checking item:", fullPath)
-
-	-- Check if the item is a directory
-	if love.filesystem.getInfo(fullPath, "directory") then
-		-- Assuming you want to load mods from subdirectories
-		local modName = item
-		print("Attempting to load mod:", modName)
-
-		-- Load the mod
-		local success, mod = pcall(require, "mods." .. modName .. "." .. modName)
-
-		-- Check if the mod loaded successfully
-		if success then
-			print("Mod loaded successfully:", modName)
-			-- Assuming the mod has an initialization function
-			if mod.initialize then
-				mod.initialize()
-			end
-		else
-			print("Failed to load mod:", modName)
-			print("Error:", mod)
-		end
-	end
-end
 
 --profiling
 ProFi = require("ProFi")
@@ -74,9 +38,11 @@ _JPROFILER = require("libs/jprofiler/jprof")
 --5 : use this command : love . LuaCraft _JPROFILER.mpack and you will see the viewer
 --init
 require("src/init/structureinit")
+
+LoadMods()
+
 gamestate = "MainMenu"
 gameSceneInstance = nil
-globalRenderDistance = nil
 
 enableProfiler = false
 
@@ -87,61 +53,12 @@ gameplayingpausemenu = nil
 playinggamesettings = nil
 worldCreationBackground = nil
 
-globalVSync = love.window.getVSync()
-
-function toggleVSync()
-	globalVSync = not globalVSync
-	love.window.setVSync(globalVSync and 1 or 0)
-
-	-- Load current contents of config.conf file
-	local content, size = love.filesystem.read("config.conf")
-
-	-- Update vsync value in content
-	content = content:gsub("vsync=%d", "vsync=" .. (globalVSync and "1" or "0"))
-
-	-- Rewrite config.conf file with updated content
-	love.filesystem.write("config.conf", content)
-end
-
-function renderdistanceSetting()
-	-- Load current contents of config.conf file
-	local content, size = love.filesystem.read("config.conf")
-
-	-- Increment the value of globalRenderDistance by 5
-	globalRenderDistance = globalRenderDistance + 5
-
-	-- Check if the value exceeds 25, reduce it to 5
-	if globalRenderDistance > 25 then
-		globalRenderDistance = 5
-	end
-
-	-- Update renderdistance value in content using regular expression
-	content = content:gsub("renderdistance=(%d+)", "renderdistance=" .. globalRenderDistance)
-
-	-- Rewrite config.conf file with updated content
-	love.filesystem.write("config.conf", content)
-end
-
 function love.load()
 	_JPROFILER.push("frame")
 	_JPROFILER.push("Mainload")
 	ModLoaderInitALL()
 	love.filesystem.setIdentity("LuaCraft")
-	if globalRenderDistance == nil then
-		-- Read the config file
-		local content = love.filesystem.read("config.conf")
-
-		-- Extract value
-		local renderDistance = tonumber(content:match("renderdistance=(%d+)")) -- Make sure the key is lowercase "renderdistance"
-
-		-- If no value in file, use default value
-		if not renderDistance then
-			renderDistance = 5
-		end
-
-		-- Set global variable
-		globalRenderDistance = renderDistance
-	end
+	SettingsHandlingInit()
 
 	if enableProfiler then
 		ProFi:start()
@@ -152,29 +69,6 @@ function love.load()
 	playinggamesettings = lg.newImage("resources/assets/backgrounds/playinggamesettings.png")
 	worldCreationBackground = lg.newImage("resources/assets/backgrounds/WorldCreationBackground.png")
 
-	if love.filesystem.getInfo("config.conf") then
-		local content, size = love.filesystem.read("config.conf")
-
-		local vsyncValue = content:match("vsync=(%d)")
-		if vsyncValue then
-			love.window.setVSync(tonumber(vsyncValue))
-		end
-
-		local renderdistanceValue = content:match("renderdistance=(%d)")
-
-		if not renderdistanceValue then
-			-- The renderdistance value does not exist, add the default value of 5
-			renderdistanceValue = "5"
-
-			-- Add the new line in the config.conf file only if it does not already exist
-			if not content:match("renderdistance=") then
-				content = content .. "\nrenderdistance=" .. renderdistanceValue
-
-				-- Update config.conf file with new value
-				love.filesystem.write("config.conf", content)
-			end
-		end
-	end
 	_JPROFILER.pop("Mainload")
 	_JPROFILER.pop("frame")
 end

mods/ExampleMod/structures/generatepillar.lua
@@ -17,7 +17,7 @@ function ExampleMod_generatePillarEveryChunks(chunk, value)
 		local pillarIndex = pillarX + size * pillarZ + size * size * pillarY
 		datapointer[pillarIndex] = value
 	end
-	--print("generatePillarEveryChunks" .. pillarX .. pillarYStart .. pillarZ)
+	--LuaCraftPrint("generatePillarEveryChunks" .. pillarX .. pillarYStart .. pillarZ)
 	_JPROFILER.pop("GameScene:update(generatePillarEveryChunks)")
 end
 
@@ -37,7 +37,7 @@ function ExampleMod_generatePillarAtFixedPosition(scene, x, y, z, value)
 		end
 	end
 
-   _JPROFILER.pop("GameScene:update(generatePillarAtFixedPosition)")
+	_JPROFILER.pop("GameScene:update(generatePillarAtFixedPosition)")
 end
 
 function ExampleMod_generatePillarAtRandomLocation(scene, value)

src/init/structureinit.lua
@@ -23,7 +23,7 @@ function generateStructuresatRandomLocation(scene, value)
 			structureGenerator(scene, value)
 			local blockKey = ("%d/%d/%d"):format(x, y, z)
 			StructureMap[blockKey] = true
-			print("Pilier généré avec succès à la position :", x, y, z)
+			LuaCraftPrint("Pilier généré avec succès à la position :", x, y, z)
 			scene:requestRemesh(scene:getChunkFromWorld(x, y, z), true)
 		end
 	end
@@ -76,7 +76,7 @@ function generateStructuresatFixedPositions(scene, size)
 		generatorFunc(scene, storedX, storedY, storedZ, 1)
 		local blockKey = ("%d/%d/%d"):format(storedX, storedY, storedZ)
 		StructureMap[blockKey] = true
-		print("Structure générée avec succès à la position :", storedX, storedY, storedZ)
+		LuaCraftPrint("Structure générée avec succès à la position :", storedX, storedY, storedZ)
 	end
 
 	_JPROFILER.pop("generateStructuresatFixedPositions")

src/modloader/modloaderinit.lua
@@ -1,3 +1,45 @@
+function LoadMods()
+	_JPROFILER.push("LoadMods")
+	-- ModsRequireIteration
+	-- Specify the path to the mods directory
+	local modsDirectory = "mods/"
+
+	-- Print debugging information
+	LuaCraftPrint("Checking mods directory:", modsDirectory)
+
+	-- Iterate over all items in the mods directory
+	local items = love.filesystem.getDirectoryItems(modsDirectory)
+	for _, item in ipairs(items) do
+		local fullPath = modsDirectory .. item
+
+		-- Print debugging information
+		LuaCraftPrint("Checking item:", fullPath)
+
+		-- Check if the item is a directory
+		if love.filesystem.getInfo(fullPath, "directory") then
+			-- Assuming you want to load mods from subdirectories
+			local modName = item
+			LuaCraftPrint("Attempting to load mod:", modName)
+
+			-- Load the mod
+			local success, mod = pcall(require, "mods." .. modName .. "." .. modName)
+
+			-- Check if the mod loaded successfully
+			if success then
+				LuaCraftPrint("Mod loaded successfully:", modName)
+				-- Assuming the mod has an initialization function
+				if mod.initialize then
+					mod.initialize()
+				end
+			else
+				LuaCraftPrint("Failed to load mod:", modName)
+				LuaCraftPrint("Error:", mod)
+			end
+		end
+	end
+	_JPROFILER.pop("LoadMods")
+end
+
 function ModLoaderInitALL()
 	_JPROFILER.push("ModLoaderInitALL")
 	--InitStructureConfigurationOLD()
@@ -17,32 +59,31 @@ function InitStructureConfigurationNEW()
 
 	local generateStructuresatRandomLocation = getFunctionsByTag("generateStructuresatRandomLocation")
 	for _, func in ipairs(generateStructuresatRandomLocation) do
-		print("Random Location Generator found:", func)
+		LuaCraftPrint("Random Location Generator found:", func)
 		Config:setRandomLocationStructureGenerator(func)
 	end
-
 
 	local generateStructuresInPlayerRange = getFunctionsByTagWithXYZ("generateStructuresInPlayerRange")
 	for _, entry in ipairs(generateStructuresInPlayerRange) do
 		local func, params = entry.func, entry.params
-		print("Fixed Position Generator found:")
-		print("Function:", func)
-		print("Parameters:")
-		print("  x:", params.x)
-		print("  y:", params.y)
-		print("  z:", params.z)
+		LuaCraftPrint("Fixed Position Generator found:")
+		LuaCraftPrint("Function:", func)
+		LuaCraftPrint("Parameters:")
+		LuaCraftPrint("  x:", params.x)
+		LuaCraftPrint("  y:", params.y)
+		LuaCraftPrint("  z:", params.z)
 
 		Config:setFixedPositionStructureGeneratorUsingPlayerRangeWithXYZ(func, params.x, params.y, params.z)
 	end
 	local generateStructuresatFixedPositions = getFunctionsByTagWithXYZ("generateStructuresatFixedPositions")
 	for _, entry in ipairs(generateStructuresatFixedPositions) do
 		local func, params = entry.func, entry.params
-		print("Fixed Position Generator found:")
-		print("Function:", func)
-		print("Parameters:")
-		print("  x:", params.x)
-		print("  y:", params.y)
-		print("  z:", params.z)
+		LuaCraftPrint("Fixed Position Generator found:")
+		LuaCraftPrint("Function:", func)
+		LuaCraftPrint("Parameters:")
+		LuaCraftPrint("  x:", params.x)
+		LuaCraftPrint("  y:", params.y)
+		LuaCraftPrint("  z:", params.z)
 
 		Config:setFixedPositionStructureGeneratorUsingIsChunkFullyGeneratedWithXYZ(func, params.x, params.y, params.z)
 	end

src/utils/settingshandling.lua
@@ -1,0 +1,154 @@
+globalRenderDistance = nil
+
+globalVSync = love.window.getVSync()
+
+local settingshandlingintialized = 0
+function reloadConfig()
+	if not love.filesystem.getInfo("config.conf") then
+		return
+	end
+
+	local content, size = love.filesystem.read("config.conf")
+
+	-- Mettez à jour globalVSync
+    local vsyncValue = content:match("vsync=(%w+)")
+    if vsyncValue then
+        globalVSync = vsyncValue:lower() == "true"
+        love.window.setVSync(globalVSync and 1 or 0)
+    end
+
+	-- Mettez à jour globalRenderDistance
+	-- local renderdistanceValue = content:match("renderdistance=(%d)")
+	-- if renderdistanceValue then
+	--     globalRenderDistance = tonumber(renderdistanceValue)
+	-- end
+
+	-- Mettez à jour EnableLuaCraftPrints
+	local printValue = content:match("luacraftprint=(%w+)")
+	if printValue then
+		EnableLuaCraftPrints = printValue:lower() == "true"
+	end
+end
+function toggleVSync()
+    globalVSync = not globalVSync
+    love.window.setVSync(globalVSync and 1 or 0)
+
+    -- Load current contents of config.conf file
+    local content, size = love.filesystem.read("config.conf")
+
+    -- Update vsync value in content
+    content = content:gsub("vsync=%w+", "vsync=" .. (globalVSync and "true" or "false"))
+
+    -- Rewrite config.conf file with updated content
+    love.filesystem.write("config.conf", content)
+end
+
+function renderdistanceSetting()
+	-- Load current contents of config.conf file
+	local content, size = love.filesystem.read("config.conf")
+
+	-- Increment the value of globalRenderDistance by 5
+	globalRenderDistance = globalRenderDistance + 5
+
+	-- Check if the value exceeds 25, reduce it to 5
+	if globalRenderDistance > 25 then
+		globalRenderDistance = 5
+	end
+
+	-- Update renderdistance value in content using regular expression
+	content = content:gsub("renderdistance=(%d+)", "renderdistance=" .. globalRenderDistance)
+
+	-- Rewrite config.conf file with updated content
+	love.filesystem.write("config.conf", content)
+end
+
+function printSettings()
+	EnableLuaCraftPrints = not EnableLuaCraftPrints
+
+	-- Load current contents of config.conf file
+	local content, size = love.filesystem.read("config.conf")
+
+	-- Update print value in content
+	local printValue = EnableLuaCraftPrints and "true" or "false"
+	content = content:gsub("luacraftprint=%w+", "luacraftprint=" .. printValue)
+
+	-- Rewrite config.conf file with updated content
+	love.filesystem.write("config.conf", content)
+end
+
+function SettingsHandlingInit()
+    reloadConfig()
+    if globalRenderDistance == nil then
+        -- Read the config file
+        local content = love.filesystem.read("config.conf")
+
+        -- Extract value
+        local renderDistance = tonumber(content:match("renderdistance=(%d+)")) -- Make sure the key is lowercase "renderdistance"
+
+        -- If no value in file, use default value
+        if not renderDistance then
+            renderDistance = 5
+        end
+
+        -- Set global variable
+        globalRenderDistance = renderDistance
+    end
+
+    if love.filesystem.getInfo("config.conf") then
+        local content, size = love.filesystem.read("config.conf")
+
+        local vsyncValue = content:match("vsync=(%d)")
+        if vsyncValue then
+            love.window.setVSync(tonumber(vsyncValue))
+        end
+
+        local renderdistanceValue = content:match("renderdistance=(%d)")
+
+        local printValue = content:match("luacraftprint=(%d)")
+
+        if not renderdistanceValue then
+            -- The renderdistance value does not exist, add the default value of 5
+            renderdistanceValue = "5"
+
+            -- Add the new line in the config.conf file only if it does not already exist
+            if not content:match("renderdistance=") then
+                content = content .. "\nrenderdistance=" .. renderdistanceValue
+
+                -- Update config.conf file with new value
+                love.filesystem.write("config.conf", content)
+            end
+        end
+
+        if not vsyncValue then
+            vsyncValue = 1
+            if not content:match("vsync=") then
+                content = content .. "\nvsync=" .. vsyncValue
+                love.filesystem.write("config.conf", content)
+            end
+        end
+        if not printValue then
+            printValue = false
+            if not content:match("luacraftprint=") then
+                EnableLuaCraftPrints = false
+                content = content .. "\nluacraftprint=" .. tostring(printValue)
+                love.filesystem.write("config.conf", content)
+            end
+        end
+    end
+    settingshandlingintialized = 1
+end
+function getLuacraftPrintValue()
+	if not love.filesystem.read("config.conf") and settingshandlingintialized == 0 then
+		return
+	end
+	local content, size = love.filesystem.read("config.conf")
+	print(content)
+	return content:match("luacraftprint=(%d)")
+end
+
+EnableLuaCraftPrints = getLuacraftPrintValue()
+function LuaCraftPrint(...)
+	if EnableLuaCraftPrints then
+		print(...)
+	end
+end

commit e3620c0413fddb9154c18084bf30a94cc9b80674

Fix vsync v=mistakes introduced by commit :  fbfb5bfccf78fca05c550751eb921e0c83815903
https://github.com/quentin452/LuaCraft/commit/fbfb5bfccf78fca05c550751eb921e0c83815903

src/menus/mainmenusettings.lua
@@ -35,8 +35,8 @@ function drawMainMenuSettings()
 
 		local choiceText = _MainMenuSettings.choice[n]
 		if n == 1 then
-			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%d)")
-			if vsyncValue and  printValue:lower() == "true" then
+			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%w+)")
+			if vsyncValue and vsyncValue:lower() == "true" then
 				choiceText = choiceText .. " X"
 			end
 		end

src/menus/playinggamesettings.lua
@@ -35,8 +35,8 @@ function drawPlayingMenuSettings()
 		local choiceText = _PlayingGameSettings.choice[n]
 		-- Ajout de la condition pour activer ou désactiver le "X"
 		if n == 1 then
-			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%d)")
-			if vsyncValue and printValue:lower() == "true" then
+			local vsyncValue = love.filesystem.read("config.conf"):match("vsync=(%w+)")
+			if vsyncValue and vsyncValue:lower() == "true" then
 				choiceText = choiceText .. " X"
 			end
 		end