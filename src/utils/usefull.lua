75fce067037c53a4dc7dc37903e0a0b85fc0a1c0
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-02-24 08:15:38 +0100

    optimize usefull.lua functions

--- src/utils/usefull.lua
+++ src/utils/usefull.lua
@@ -14,7 +14,7 @@ function drawColorString(Pstring, Px, Py)
 	local defaultColor = { 255, 255, 255 }
 	local currentColor = defaultColor
 
-	lg.setColor(unpack(currentColor))
+	lg.setColor(currentColor)
 
 	local i = 1
 	local len = #Pstring
@@ -25,47 +25,47 @@ function drawColorString(Pstring, Px, Py)
 		if c == "%" then
 			local colorDigit = string.sub(Pstring, i + 1, i + 1)
 			currentColor = colorMap[tostring(colorDigit)] or defaultColor
-			lg.setColor(unpack(currentColor))
+			lg.setColor(currentColor)
 			i = i + 2 -- skip both '%' and the color digit
 		else
 			lg.print(c, rx, ry)
-			if
+			local fontWidth = (
 				gamestate == "MainMenu"
 				or gamestate == "MainMenuSettings"
 				or gamestate == "GamePausing"
 				or gamestate == "PlayingGameSettings"
-			then
-				rx = rx + font25:getWidth(c)
-			end
-			if gamestate == "WorldCreationMenu" or gamestate == "PlayingGame" then
-				rx = rx + font15:getWidth(c)
-			end
+			)
+					and font25
+				or ("WorldCreationMenu" or gamestate == "PlayingGame") and font15
+			rx = rx + fontWidth:getWidth(c)
 			i = i + 1
 		end
 	end
-
+	lg.setColor(defaultColor)
 	_JPROFILER.pop("drawColorString")
 end
 
+local fontTable = {
+	MainMenu = font25,
+	MainMenuSettings = font25,
+	GamePausing = font25,
+	PlayingGameSettings = font25,
+	WorldCreationMenu = font15,
+	PlayingGame = font15,
+}
+
 function setFont()
 	_JPROFILER.push("setFont")
-	if
-		gamestate == "MainMenu"
-		or gamestate == "MainMenuSettings"
-		or gamestate == "GamePausing"
-		or gamestate == "PlayingGameSettings"
-	then
-		lg.setFont(font25)
-	end
-	if gamestate == "WorldCreationMenu" or gamestate == "PlayingGame" then
-		lg.setFont(font15)
+	local selectedFont = fontTable[gamestate]
+	if selectedFont then
+		lg.setFont(selectedFont)
 	end
 	_JPROFILER.pop("setFont")
 end
 
 function IsStructureIsGenerated(x, y, z)
 	_JPROFILER.push("IsStructureIsGenerated")
-	local blockKey = ("%d/%d/%d"):format(x, y, z)
+	local blockKey = string.format("%d/%d/%d", x, y, z)
 	_JPROFILER.pop("IsStructureIsGenerated")
 	return StructureMap[blockKey]
 end
@@ -74,9 +74,5 @@ function isChunkFullyGenerated(scene, chunkX, chunkY, chunkZ)
 	local chunkKey = ("%d/%d/%d"):format(chunkX, chunkY, chunkZ)
 	local chunk = scene.chunkMap[chunkKey]
 
-	if chunk and chunk.data then
-		return true
-	else
-		return false
-	end
+	return chunk and chunk.data
 end
