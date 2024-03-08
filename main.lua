30df743825fd5cc514f42f3f5b7aabdd4f3993de
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-08 05:04:33 +0100

    Customize love.run (don't need my love2d fork anymore)

--- main.lua
+++ main.lua
@@ -76,7 +76,57 @@ modelalreadycreated = 0
 ChunkBorderAlreadyCreated = 0
 
 hudTimeLeft = 0
+function love.run()
+	if love.load then
+		love.load(love.arg.parseGameArguments(arg), arg)
+	end
 
+	-- We don't want the first frame's dt to include time taken by love.load.
+	if love.timer then
+		love.timer.step()
+	end
+
+	local dt = 0
+
+	-- Main loop time.
+	return function()
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a, b, c, d, e, f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a or 0
+					end
+				end
+				love.handlers[name](a, b, c, d, e, f)
+			end
+		end
+
+		-- Update dt, as we'll be passing it to update
+		if love.timer then
+			dt = love.timer.step()
+		end
+
+		-- Call update and draw
+		if love.update then
+			love.update(dt)
+		end -- will pass 0 if love.timer is disabled
+
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.origin()
+			love.graphics.clear(love.graphics.getBackgroundColor())
+
+			if love.draw then
+				love.draw()
+			end
+
+			love.graphics.present()
+		end
+
+		--		if love.timer then love.timer.sleep(0.001) end
+	end
+end
 function love.load()
 	_JPROFILER.push("frame")
 	_JPROFILER.push("Mainload")
