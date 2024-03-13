0cb7218e8b49b47c0e06498cd5ba8dcd4af17a9d
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-13 01:52:08 +0100

    lighting Query refactoring

--- src/world/lighting.lua
+++ src/world/lighting.lua
@@ -33,200 +33,216 @@ function LightingUpdate()
 	LightingRemovalQueue = {}
 	LightingQueue = {}
 end
-local function NewSunlightAddition(x, y, z, value)
-	local queue = {}
-	table.insert(queue, { x = x, y = y, z = z, value = value })
-
-	while #queue > 0 do
-		local item = table.remove(queue, 1)
-		local cget, cx, cy, cz = GetChunk(item.x, item.y, item.z)
-		if cget then
-			local val = cget:getVoxel(cx, cy, cz)
-			local dat = cget:getVoxelFirstData(cx, cy, cz)
-			if item.value >= 0 and TileSemiLightable(val) and dat < item.value then
-				cget:setVoxelFirstData(cx, cy, cz, item.value)
-				for _, dir in ipairs(SIXDIRECTIONS) do
-					local nx, ny, nz = item.x + dir.x, item.y + dir.y, item.z + dir.z
-					table.insert(queue, { x = nx, y = ny, z = nz, value = item.value - 1 })
-				end
-			end
-		end
-	end
-end
 
 local function NewSunlightForceAddition(x, y, z, value)
-	local t = { x = x, y = y, z = z, value = value }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val = cget:getVoxel(cx, cy, cz)
-		if self.value >= 0 and TileSemiLightable(val) then
-			cget:setVoxelFirstData(cx, cy, cz, self.value)
-			for _, dir in ipairs(SIXDIRECTIONS) do
-				NewSunlightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
-			end
-		end
-	end
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewSunlightForceAddition" }
+	t.query = LightningQueries(t)
 	LightingQueueAdd(t)
 end
 local function NewSunlightAdditionCreation(x, y, z)
-	local t = { x = x, y = y, z = z }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val = cget:getVoxel(cx, cy, cz)
-		local dat = cget:getVoxelFirstData(cx, cy, cz)
-		if TileSemiLightable(val) and dat > 0 then
-			NewSunlightForceAddition(self.x, self.y, self.z, dat)
-		end
-	end
+	local t = { x = x, y = y, z = z, querytype = "NewSunlightAdditionCreation" }
+	t.query = LightningQueries(t)
 	LightingQueueAdd(t)
 end
 
 local function NewSunlightDownAddition(x, y, z, value)
-	local t = { x = x, y = y, z = z, value = value }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val = cget:getVoxel(cx, cy, cz)
-		local dat = cget:getVoxelFirstData(cx, cy, cz)
-		if TileLightable(val) and dat <= self.value then
-			cget:setVoxelFirstData(cx, cy, cz, self.value)
-			NewSunlightDownAddition(self.x, self.y - 1, self.z, self.value)
-			for _, dir in ipairs(SIXDIRECTIONS) do
-				NewSunlightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
-			end
-		end
-	end
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewSunlightDownAddition" }
+	t.query = LightningQueries(t)
 	LightingQueueAdd(t)
 end
-local function NewLocalLightAddition(x, y, z, value)
-	local queue = {}
-	table.insert(queue, { x = x, y = y, z = z, value = value })
 
-	while #queue > 0 do
-		local item = table.remove(queue, 1)
-		local chunk = GetChunk(item.x, item.y, item.z)
-		if chunk then
-			local cx, cy, cz = Localize(item.x, item.y, item.z)
-			local val, dis, dat = chunk:getVoxel(cx, cy, cz)
-			if TileSemiLightable(val) and dat < item.value then
-				chunk:setVoxelSecondData(cx, cy, cz, item.value)
-				if item.value > 1 then
-					for _, dir in ipairs(SIXDIRECTIONS) do
-						local nx, ny, nz = item.x + dir.x, item.y + dir.y, item.z + dir.z
-						table.insert(queue, { x = nx, y = ny, z = nz, value = item.value - 1 })
-					end
-				end
-			end
-		end
-	end
-end
 local function NewLocalLightForceAddition(x, y, z, value)
-	local t = { x = x, y = y, z = z, value = value }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val, dis, dat = cget:getVoxel(cx, cy, cz)
-		if self.value >= 0 and TileSemiLightable(val) then
-			cget:setVoxelSecondData(cx, cy, cz, self.value)
-			for _, dir in ipairs(SIXDIRECTIONS) do
-				NewLocalLightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
-			end
-		end
-	end
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewLocalLightForceAddition" }
+	t.query = LightningQueries(t)
 	LightingQueueAdd(t)
 end
-local function NewSunlightSubtraction(x, y, z, value)
-	local queue = {}
-	table.insert(queue, { x = x, y = y, z = z, value = value })
 
-	while #queue > 0 do
-		local item = table.remove(queue, 1)
-		local cget, cx, cy, cz = GetChunk(item.x, item.y, item.z)
-
-		if cget then
-			local val = cget:getVoxel(cx, cy, cz)
-			local fget = cget:getVoxelFirstData(cx, cy, cz)
-
-			if fget > 0 and item.value >= 0 and TileSemiLightable(val) then
-				if fget < item.value then
-					cget:setVoxelFirstData(cx, cy, cz, Tiles.AIR_Block.id)
-					for _, dir in ipairs(SIXDIRECTIONS) do
-						local nx, ny, nz = item.x + dir.x, item.y + dir.y, item.z + dir.z
-						table.insert(queue, { x = nx, y = ny, z = nz, value = fget })
-					end
-				else
-					NewSunlightForceAddition(item.x, item.y, item.z, fget)
-				end
-			end
-		end
-	end
-end
-
-local function NewSunlightDownSubtraction(x, y, z)
-	local queue = {}
-	table.insert(queue, { x = x, y = y, z = z })
-
-	while #queue > 0 do
-		local item = table.remove(queue, 1)
-		if TileSemiLightable(GetVoxel(item.x, item.y, item.z)) then
-			SetVoxelFirstData(item.x, item.y, item.z, Tiles.AIR_Block.id)
-			table.insert(queue, { x = item.x, y = item.y - 1, z = item.z })
-			for _, dir in ipairs(SIXDIRECTIONS) do
-				local nx, ny, nz = item.x + dir.x, item.y + dir.y, item.z + dir.z
-				NewSunlightSubtraction(nx, ny, nz, LightSources[15])
-			end
-		end
-	end
-end
 local function NewLocalLightSubtraction(x, y, z, value)
-	local t = { x = x, y = y, z = z, value = value }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val, dat = cget:getVoxel(cx, cy, cz)
-		local fget = cget:getVoxelSecondData(cx, cy, cz)
-		if fget > 0 and self.value >= 0 and TileSemiLightable(val) then
-			if fget < self.value then
-				cget:setVoxelSecondData(cx, cy, cz, 0)
-				for _, dir in ipairs(SIXDIRECTIONS) do
-					local nx, ny, nz = self.x + dir.x, self.y + dir.y, self.z + dir.z
-					NewLocalLightSubtraction(nx, ny, nz, fget)
-				end
-			else
-				NewLocalLightForceAddition(self.x, self.y, self.z, fget)
-			end
-			return false
-		end
-	end
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewLocalLightSubtraction" }
+	t.query = LightningQueries(t)
 	LightingRemovalQueueAdd(t)
 end
 local function NewLocalLightAdditionCreation(x, y, z)
-	local t = { x = x, y = y, z = z }
-	t.query = function(self)
-		local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
-		if cget == nil then
-			return
-		end
-		local val, dis, dat = cget:getVoxel(cx, cy, cz)
-		if TileSemiLightable(val) and dat > 0 then
-			NewLocalLightForceAddition(self.x, self.y, self.z, dat)
-		end
-	end
+	local t = { x = x, y = y, z = z, querytype = "NewLocalLightAdditionCreation" }
+	t.query = LightningQueries(t)
+	LightingQueueAdd(t)
+end
+function NewSunlightAddition(x, y, z, value)
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewSunlightAddition" }
+	t.query = LightningQueries(t)
 	LightingQueueAdd(t)
 end
 
+local function NewLocalLightAddition(x, y, z, value)
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewLocalLightAddition" }
+	t.query = LightningQueries(t)
+	LightingQueueAdd(t)
+end
+local function NewSunlightSubtraction(x, y, z, value)
+	local t = { x = x, y = y, z = z, value = value, querytype = "NewSunlightSubtraction" }
+	t.query = LightningQueries(t)
+	LightingRemovalQueueAdd(t)
+end
+local function NewSunlightDownSubtraction(x, y, z)
+	local t = { x = x, y = y, z = z, querytype = "NewSunlightDownSubtraction" }
+	t.query = LightningQueries(t)
+	LightingRemovalQueueAdd(t)
+end
+
+function LightningQueries(self)
+	if self.querytype == "NewSunlightForceAddition" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val = cget:getVoxel(cx, cy, cz)
+			if self.value >= 0 and TileSemiLightable(val) then
+				cget:setVoxelFirstData(cx, cy, cz, self.value)
+				for _, dir in ipairs(SIXDIRECTIONS) do
+					NewSunlightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
+				end
+			end
+		end
+	elseif self.querytype == "NewSunlightAdditionCreation" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val = cget:getVoxel(cx, cy, cz)
+			local dat = cget:getVoxelFirstData(cx, cy, cz)
+			if TileSemiLightable(val) and dat > 0 then
+				NewSunlightForceAddition(self.x, self.y, self.z, dat)
+			end
+		end
+	elseif self.querytype == "NewSunlightDownAddition" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val = cget:getVoxel(cx, cy, cz)
+			local dat = cget:getVoxelFirstData(cx, cy, cz)
+			if TileLightable(val) and dat <= self.value then
+				cget:setVoxelFirstData(cx, cy, cz, self.value)
+				NewSunlightDownAddition(self.x, self.y - 1, self.z, self.value)
+				for _, dir in ipairs(SIXDIRECTIONS) do
+					NewSunlightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
+				end
+			end
+		end
+	elseif self.querytype == "NewLocalLightForceAddition" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val, dis, dat = cget:getVoxel(cx, cy, cz)
+			if self.value >= 0 and TileSemiLightable(val) then
+				cget:setVoxelSecondData(cx, cy, cz, self.value)
+				for _, dir in ipairs(SIXDIRECTIONS) do
+					NewLocalLightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
+				end
+			end
+		end
+	elseif self.querytype == "NewLocalLightSubtraction" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val, dat = cget:getVoxel(cx, cy, cz)
+			local fget = cget:getVoxelSecondData(cx, cy, cz)
+			if fget > 0 and self.value >= 0 and TileSemiLightable(val) then
+				if fget < self.value then
+					cget:setVoxelSecondData(cx, cy, cz, 0)
+					for _, dir in ipairs(SIXDIRECTIONS) do
+						local nx, ny, nz = self.x + dir.x, self.y + dir.y, self.z + dir.z
+						NewLocalLightSubtraction(nx, ny, nz, fget)
+					end
+				else
+					NewLocalLightForceAddition(self.x, self.y, self.z, fget)
+				end
+				return false
+			end
+		end
+	elseif self.querytype == "NewLocalLightAdditionCreation" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val, dis, dat = cget:getVoxel(cx, cy, cz)
+			if TileSemiLightable(val) and dat > 0 then
+				NewLocalLightForceAddition(self.x, self.y, self.z, dat)
+			end
+		end
+	elseif self.querytype == "NewSunlightAddition" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val = cget:getVoxel(cx, cy, cz)
+			local dat = cget:getVoxelFirstData(cx, cy, cz)
+			if self.value >= 0 and TileSemiLightable(val) and dat < self.value then
+				cget:setVoxelFirstData(cx, cy, cz, self.value)
+				for _, dir in ipairs(SIXDIRECTIONS) do
+					NewSunlightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
+				end
+			end
+		end
+	elseif self.querytype == "NewLocalLightAddition" then
+		return function()
+			local chunk = GetChunk(self.x, self.y, self.z)
+			if chunk == nil then
+				return
+			end
+			local cx, cy, cz = Localize(self.x, self.y, self.z)
+			local val, dis, dat = chunk:getVoxel(cx, cy, cz)
+			if TileSemiLightable(val) and dat < self.value then
+				chunk:setVoxelSecondData(cx, cy, cz, self.value)
+				if self.value > 1 then
+					for _, dir in ipairs(SIXDIRECTIONS) do
+						NewLocalLightAddition(self.x + dir.x, self.y + dir.y, self.z + dir.z, self.value - 1)
+					end
+				end
+			end
+		end
+	elseif self.querytype == "NewSunlightSubtraction" then
+		return function()
+			local cget, cx, cy, cz = GetChunk(self.x, self.y, self.z)
+			if cget == nil then
+				return
+			end
+			local val = cget:getVoxel(cx, cy, cz)
+			local fget = cget:getVoxelFirstData(cx, cy, cz)
+			if fget > 0 and self.value >= 0 and TileSemiLightable(val) then
+				if fget < self.value then
+					cget:setVoxelFirstData(cx, cy, cz, Tiles.AIR_Block.id)
+					for _, dir in ipairs(SIXDIRECTIONS) do
+						NewSunlightSubtraction(self.x + dir.x, self.y + dir.y, self.z + dir.z, fget)
+					end
+				else
+					NewSunlightForceAddition(self.x, self.y, self.z, fget)
+				end
+				return false
+			end
+		end
+	elseif self.querytype == "NewSunlightDownSubtraction" then
+		return function()
+			if TileSemiLightable(GetVoxel(self.x, self.y, self.z)) then
+				SetVoxelFirstData(self.x, self.y, self.z, Tiles.AIR_Block.id)
+				NewSunlightDownSubtraction(self.x, self.y - 1, self.z)
+				for _, dir in ipairs(SIXDIRECTIONS) do
+					NewSunlightSubtraction(self.x + dir.x, self.y + dir.y, self.z + dir.z, LightSources[15])
+				end
+				return true
+			end
+		end
+	end
+end
 local operationFunctions = {
 	["NewSunlightAddition"] = NewSunlightAddition,
 	["NewSunlightAdditionCreation"] = NewSunlightAdditionCreation,
