fa5084f2bd988f88b9560c77c126c39a7e585795
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-14 23:41:04 +0100

    simplify ExampleMod.initialize populate code

--- mods/ExampleMod/ExampleMod.lua
+++ mods/ExampleMod/ExampleMod.lua
@@ -8,22 +8,16 @@ function ExampleMod.initialize()
 	addFunctionToTag("chunkPopulateTag", function(self, i, height, j)
 		local xx = (self.x - 1) * ChunkSize + i
 		local zz = (self.z - 1) * ChunkSize + j
-		--generate Trees
-		if math.random() < love.math.noise(xx / 64, zz / 64) * 0.02 then
-			ExampleMod_GenerateTree(self, i, height, j)
-			self:setVoxelRaw(i, height, j, Tiles.DIRT_Block.id, LightSources[15])
-		end
-	end)
-	addFunctionToTag("chunkPopulateTag", function(self, i, height, j)
-		local xx = (self.x - 1) * ChunkSize + i
-		local zz = (self.z - 1) * ChunkSize + j
-		--generate flowers YELLOW/ROSE
 		if love.math.noise(xx / 32, zz / 32) > 0.9 and math.random() < 0.2 then
 			local min = math.min(Tiles.YELLO_FLOWER_Block.id, Tiles.ROSE_FLOWER_Block.id)
 			local max = math.max(Tiles.YELLO_FLOWER_Block.id, Tiles.ROSE_FLOWER_Block.id)
 
 			local flowerID = math.random(min, max)
 			self:setVoxelRaw(i, height + 1, j, flowerID, LightSources[15])
+		end
+		if math.random() < love.math.noise(xx / 64, zz / 64) * 0.02 then
+			ExampleMod_GenerateTree(self, i, height, j)
+			self:setVoxelRaw(i, height, j, Tiles.DIRT_Block.id, LightSources[15])
 		end
 	end)
 end
