c606150259668ad1ea45bcbc5559c05aabb4c7e8
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-25 00:25:20 +0100

    Should reduce a bit memory usage caused by caves

--- src/world/gen/caves.lua
+++ src/world/gen/caves.lua
@@ -25,9 +25,56 @@ function UpdateCaves()
 	_JPROFILER.pop("UpdateCaves")
 end
 
+local function caveQuery(cave)
+	local sinThetaCosPhi = math.sin(cave.theta) * math.cos(cave.phi)
+	local sinPhi = math.sin(cave.phi)
+	local cosThetaCosPhi = math.cos(cave.theta) * math.cos(cave.phi)
+
+	cave.x, cave.y, cave.z = cave.x + sinThetaCosPhi, cave.y + sinPhi, cave.z + cosThetaCosPhi
+
+	cave.theta = cave.theta + cave.deltaTheta * 0.2
+	cave.deltaTheta = cave.deltaTheta * 0.9 + math.random() - math.random()
+	cave.phi = cave.phi / 2 + cave.deltaPhi / 4
+	cave.deltaPhi = cave.deltaPhi * 0.75 + math.random() - math.random()
+
+	if cave.carveIndex >= cave.radius then
+		cave:carve()
+		cave.carveIndex = 0
+	end
+	cave.carveIndex = cave.carveIndex + 1
+
+	cave.lifeTimer = cave.lifeTimer - 1
+	return cave.lifeTimer > 0
+end
+
+local function caveCarve(cave)
+	if GetVoxel(cave.x, cave.y, cave.z) ~= 0 then
+		local halfRandom = math.random() / 2
+		local squaredRadius = cave.radius * cave.radius
+
+		for i = -cave.radius, cave.radius do
+			for j = -cave.radius, cave.radius do
+				for k = -cave.radius, cave.radius do
+					local distanceSquared = i * i + j * j + k * k
+					if distanceSquared + halfRandom < squaredRadius then
+						local gx, gy, gz = cave.x + i, cave.y + j, cave.z + k
+						local chunk, cx, cy, cz = GetChunk(gx, gy, gz)
+						if chunk ~= nil then
+							chunk:setVoxelRawNotSupportLight(cx, cy, cz, Tiles.AIR_Block.id)
+							if cy == chunk.heightMap[cx][cz] then
+								NewLightOperation(gx, gy, gz, LightOpe.SunDownAdd.id, LightSources[15])
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
 function NewCave(x, y, z)
 	_JPROFILER.push("NewCave")
-	local t = {
+	local cave = {
 		x = x,
 		y = y,
 		z = z,
@@ -38,61 +85,10 @@ function NewCave(x, y, z)
 		deltaPhi = 0,
 		radius = rand(2, 3, 0.1),
 		carveIndex = 0,
+		query = caveQuery,
+		carve = caveCarve,
 	}
-	t.query = function(self)
-		local sinThetaCosPhi = math.sin(self.theta) * math.cos(self.phi)
-		local sinPhi = math.sin(self.phi)
-		local cosThetaCosPhi = math.cos(self.theta) * math.cos(self.phi)
-
-		self.x, self.y, self.z = self.x + sinThetaCosPhi, self.y + sinPhi, self.z + cosThetaCosPhi
-
-		self.theta = self.theta + self.deltaTheta * 0.2
-		self.deltaTheta = self.deltaTheta * 0.9 + math.random() - math.random()
-		self.phi = self.phi / 2 + self.deltaPhi / 4
-		self.deltaPhi = self.deltaPhi * 0.75 + math.random() - math.random()
-
-		if self.carveIndex >= self.radius then
-			self:carve()
-			self.carveIndex = 0
-		end
-		self.carveIndex = self.carveIndex + 1
-
-		self.lifeTimer = self.lifeTimer - 1
-		return self.lifeTimer > 0
-	end
-
-	t.carve = function(self)
-		if GetVoxel(self.x, self.y, self.z) ~= 0 then
-			local halfRandom = math.random() / 2
-			local squaredRadius = self.radius * self.radius
-
-			for i = -self.radius, self.radius do
-				for j = -self.radius, self.radius do
-					for k = -self.radius, self.radius do
-						local distanceSquared = i * i + j * j + k * k
-						if distanceSquared + halfRandom < squaredRadius then
-							local gx, gy, gz = self.x + i, self.y + j, self.z + k
-							local chunk, cx, cy, cz = GetChunk(gx, gy, gz)
-							if chunk ~= nil then
-								chunk:setVoxelRawNotSupportLight(cx, cy, cz, Tiles.AIR_Block.id)
-								if cy == chunk.heightMap[cx][cz] then
-									NewLightOperation(gx, gy, gz, LightOpe.SunDownAdd.id, LightSources[15])
-									--[[	ThreadLightingChannel:push({"LightOperation",
-										gx,
-										gy,
-										gz,
-										LightOpe.SunDownAdd.id,
-										LightSources[15],
-									})]]
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-	CaveList[#CaveList + 1] = t
+	CaveList[#CaveList + 1] = cave
 	_JPROFILER.pop("NewCave")
-	return t
+	return cave
 end
