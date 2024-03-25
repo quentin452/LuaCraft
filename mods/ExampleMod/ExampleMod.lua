1a3150d39af8db0956e2d148cb26afbab1b05fe1
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-25 01:14:24 +0100

    Fix #83

--- mods/ExampleMod/ExampleMod.lua
+++ mods/ExampleMod/ExampleMod.lua
@@ -6,16 +6,13 @@ local ExampleMod = {}
 
 function ExampleMod.initialize()
 	addFunctionToTag("chunkPopulateTag", function(self, i, height, j)
-		local xx = (self.x - 1) * ChunkSize + i
-		local zz = (self.z - 1) * ChunkSize + j
-		if math.random(xx / 32, zz / 32) > 0.9 and math.random() < 0.2 then
+		if math.random() < 0.2 then
 			local min = math.min(Tiles.YELLO_FLOWER_Block.id, Tiles.ROSE_FLOWER_Block.id)
 			local max = math.max(Tiles.YELLO_FLOWER_Block.id, Tiles.ROSE_FLOWER_Block.id)
-
 			local flowerID = math.random(min, max)
 			self:setVoxelRawNotSupportLight(i, height + 1, j, flowerID)
 		end
-		if math.random() < math.random(xx / 64, zz / 64) * 0.02 then
+		if math.random() < 0.02 then
 			ExampleMod_GenerateTree(self, i, height, j)
 			self:setVoxelRawNotSupportLight(i, height, j, Tiles.DIRT_Block.id)
 		end
