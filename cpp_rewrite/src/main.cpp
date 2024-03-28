f7e6c8982608f3e0e1b4213543e7739e7b3b3f51
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-03-28 22:09:42 +0100

    Fix window blackscreen bugs

--- cpp_rewrite/src/main.cpp
+++ cpp_rewrite/src/main.cpp
@@ -58,7 +58,6 @@ int main() {
     }
     manager.get().update();
     manager.get().draw(window);
-    glfwSwapBuffers(window);
     // TODO FIX glfwPollEvents causing lags when moving mouse
     glfwPollEvents();
   }
